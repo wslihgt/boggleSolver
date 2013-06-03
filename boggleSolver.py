46decb126d6800b9caf975d8b1cb799f5c28a7c0
Jean-Louis Durrieu <jean-louis@durrieu.ch>
2013-06-03 15:45:47 +0200

    Initial commit of boggle solver - works fine

---
+++ boggleSolver.py
@@ -0,0 +1,228 @@
+"""a program to solve a boggle problem
+"""
+
+import numpy as np
+import string
+from dico import prefix, words
+
+alphabet = np.array([l for l in string.ascii_uppercase])
+alphabet_size = len(alphabet)
+
+extendIncrement = []
+for r in [-1, 0, 1]:
+    for c in [-1, 0, 1]:
+        extendIncrement.append([r,c])
+extendIncrement.remove([0,0])
+
+def initArray(array, rows, cols, type='num'):
+    if (not(isinstance(array, np.ndarray)) and
+        not(isinstance(array, str)) and array is not None):
+        print array
+        raise TypeError("array must be a string or an ndarray")
+    elif array is None: # redundant with 'else'
+        random_draw = np.int32(np.random.rand(rows, cols)
+                               * alphabet_size)
+        if type == 'alpha':
+            narray = alphabet[random_draw]
+        else:
+            narray = random_draw
+    elif isinstance(array, str) and len(array) == rows * cols:
+        array.upper()
+        narray = np.array([l for l in array]).reshape(rows, cols)
+        if type == 'num':
+            narray = np.int32(narray)
+        else:
+            narray = np.array(
+                [[elt.upper() for elt in row] for row in narray])
+    elif array.shape == (rows, cols):
+        narray = array
+        if type=='num':
+            narray = np.int32(narray)
+        else:
+            narray = np.array(
+                [[elt.upper() for elt in row] for row in array])
+    else: # letters is None or letters.shape != (rows, cols):
+        random_draw = np.int32(np.random.rand(rows, cols)
+                               * alphabet_size)
+        if type == 'alpha':
+            narray = alphabet[random_draw]
+        else:
+            narray = random_draw
+        
+    return narray
+
+class LetterGrid(object):
+    def __init__(self, letters='abcdefghijklmnop',
+                 points=None,
+                 bonus=None,
+                 rows=4, cols=4):
+        """initialize the grids, random if none provided
+        """
+        self.rows = rows
+        self.cols = cols
+        self.points = np.ones([rows, cols])
+        self.bonus = np.ones([rows, cols])
+        
+        ##if (not(isinstance(letters, np.ndarray)) or
+        ##    not(isinstance(letters, str))):
+        ##elif len(letters) == rows * cols:
+        ##    self.letters = np.array([l for l in letters]).reshape(rows, cols)
+        ##elif letters.shape != (rows, cols):
+        ##    self.letters = letters
+        ##else: # letters is None or letters.shape != (rows, cols):
+        ##    random_draw = np.int32(np.random.rand(rows, cols)
+        ##                           * alphabet_size)
+        ##    self.letters = alphabet[random_draw]
+        self.letters = initArray(letters, rows=rows, cols=cols, type='alpha')
+        if points is not None:
+            self.points = initArray(points, rows=rows, cols=cols, type='num')
+        if bonus is not None:
+            self.bonus = initArray(bonus, rows=rows, cols=cols, type='num')
+
+class Path(object):
+    def __init__(self, letterGrid=None, sequence=[]):
+        """a class aiming at providing wrapping operations
+        around a path in the letterGrid, keeping track of
+        the path, how it can be extended, whether it is valid,
+        etc.
+        """
+        if isinstance(letterGrid, LetterGrid):
+            self.letterGrid = letterGrid
+        else:
+            raise ValueError("raising error for now, lettergrid wrong")
+        self.sequence = sequence
+        currentPrefixDico = prefix
+        for s in self.sequence:
+            c = self.letterGrid.letters[s[0],s[1]]
+            if c not in currentPrefixDico:
+                raise ValueError(''.join([self.letterGrid.letters[s[0], s[1]]
+                                          for s in self.sequence])
+                                 + ' is not a valid word prefix')
+            currentPrefixDico = currentPrefixDico[c]
+        self.possibleLetters = currentPrefixDico
+        if 'EOW' in self.possibleLetters:
+            print ''.join([self.letterGrid.letters[s[0],s[1]]
+                           for s in self.sequence]),
+            print self.sequence
+            self.isWord = True
+        else:
+            self.isWord = False
+
+    def extend(self, ):
+        """returns a list of Paths that are valid extensions of the
+        instance path. 
+        """
+        extendedPaths = []
+        for item in self.possibleExtendToItem():
+            newSequence = list(self.sequence)
+            newSequence.append(item)
+            path = Path(letterGrid=self.letterGrid,
+                        sequence=newSequence)
+            extendedPaths.append(path)
+            
+        return extendedPaths
+
+    def isValid(self):
+        """check if this prefix exists in the dictionary
+        """
+        return False
+
+    def possibleExtendToItem(self):
+        """checks the last element of the path, and 
+        """
+        items = []
+        if len(self.sequence):
+            lastItem = self.sequence[-1]
+        else:
+            lastItem = [0,0] # np.argmax(self.letterGrid.bonus)
+        for inc in extendIncrement:
+            nextItem = [lastItem[0] + inc[0],
+                        lastItem[1] + inc[1]]
+            if (nextItem[0]>=0 and nextItem[0] < self.letterGrid.rows
+                and nextItem[1]>=0 and nextItem[1] < self.letterGrid.cols
+                and nextItem not in self.sequence
+                and self.letterGrid.letters[nextItem[0], nextItem[1]] in
+                self.possibleLetters):
+                items.append(nextItem)
+        return items
+    
+    def __string__(self):
+        print self.word(), self.score()
+        print self.onTheGrid()
+    
+    def word(self):
+        return ''.join([self.letterGrid.letters[s[0], s[1]]
+                       for s in self.sequence])
+
+    def onTheGrid(self):
+        """graphical representation of the path on the grid
+        """
+        stringPath = ''
+        prefix = '        '
+        gridPath = np.zeros(self.letterGrid.letters.shape)
+        ##s = ('          X  X  X  X\n' +
+        ##     '          X  X  X  X\n' +
+        ##     '          X  X  X  X\n' +
+        ##     '          X  X  X  X\n')
+        
+        for ns, s in enumerate(self.sequence):
+            gridPath[s[0],s[1]] = ns + 1
+            
+        for row in range(self.letterGrid.rows):
+            stringPath += prefix
+            stringPath += ''.join([('%3d' %g) for g in gridPath[row]])
+            stringPath += '\n'
+        
+        return stringPath
+    
+    def score(self):
+        """compute the score for the word
+        """
+        score = 0
+        totMul = 1
+        for s in self.sequence:
+            score += self.letterGrid.points[s[0],s[1]]
+            totMul *= self.letterGrid.bonus[s[0],s[1]]
+        return score * totMul
+
+class Ruzzle(object):
+    def __init__(self, letters=None,):
+        self.letterGrid = LetterGrid(letters=letters, rows=4, cols=4)
+
+    def listAllWords(self):
+        """Take the letter Grid, compute all words and list them
+        in order of 
+        """
+        # words can start from any place, start paths for each:
+        
+        pathList = []
+        
+        for r in range(self.letterGrid.rows):
+            for c in range(self.letterGrid.cols):
+                pathList.append(Path(self.letterGrid,
+                                     sequence=[[r,c]]))
+                
+        words = []
+        while(len(pathList)):
+            paths = list(pathList)
+            pathList = []
+            for n, p in enumerate(paths):
+                if p.isWord:
+                    words.append(p)
+                pathList.extend(p.extend())
+                
+        scores = [w.score() for w in words]
+        sortIndex = np.argsort(scores)
+        sortedWords = []
+        for i in sortIndex:
+            sortedWords.append(words[i])
+            print words[i].word(), words[i].score()
+            print words[i].onTheGrid()
+            
+        return sortedWords
+
+
+##if __name__=='__main__':
+##    import optparse
+    
+##    parser = optparse.Parser()
---
+++ dico.py
@@ -0,0 +1,48 @@
+"""load dictionaries of words, modify to fit the required language
+"""
+
+import numpy as np
+import unicodedata as ud
+
+def rmdiacritics(char):
+    '''
+    Return the base character of char, by "removing" any
+    diacritics like accents or curls and strokes and the like.
+    '''
+    desc = ud.name(unicode(char))
+    cutoff = desc.find(' WITH ')
+    if cutoff != -1:
+        desc = desc[:cutoff]
+    return ud.lookup(desc)
+
+def strip_accents(s):
+   return ''.join(c for c in ud.normalize('NFD', s)
+                  if ud.category(c) != 'Mn')
+
+def addWordToPrefix(prefix, word):
+    currentdico = prefix
+    # capword = [rmdiacritics(c).upper() for c in word]
+    #capword = [strip_accents(c).upper() for c in word]
+    capword = strip_accents(unicode(word, 'latin1')).upper()
+    ##print word, capword
+    for c in capword:
+        if c not in currentdico:
+            currentdico[c] = {}
+        currentdico = currentdico[c]
+    currentdico['EOW'] = None
+
+dicofilename = 'beep.txt' ## English
+# dicofilename = 'liste.de.mots.francais.frgut.txt'
+txtfile = open(dicofilename)
+
+lines = txtfile.readlines()
+txtfile.close()
+
+words = []
+prefix = {}
+for l in lines:
+    word = l.strip().split('\t')[0]
+    words.append(word)
+    
+    addWordToPrefix(prefix, word)
+
